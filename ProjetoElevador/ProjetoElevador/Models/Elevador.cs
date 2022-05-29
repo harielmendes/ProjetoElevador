11da77d893328c5294a50455f8ccf8f9a2dc1943
harielmendes <104396842+harielmendes@users.noreply.github.com>
2022-05-29 13:32:18 -0300

    Add files via upload

---
+++ ProjetoElevador/ProjetoElevador/Models/Elevador.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    public class Elevador
+    {
+        public int andarAtual = 0; //o elevador vai sair do térreo, andar 0.
+        public int totalAndares;
+        public int numeroPessoas = 0; //o elevador vai começar vazio.
+        public int lotacao;
+
+        public void Inicializar() //esta função pedirá ao usuário alguns parâmetros de início.
+        {
+            // perguntar ao usuário a capacidade máxima do elevador (quantas pessoas vão caber dentro do elevador)
+            Console.WriteLine("Informe qual a capacidade máxima do elevador: ");
+            lotacao = int.Parse(Console.ReadLine());
+
+            // perguntar ao usuário a quantidade de andares do prédio
+            Console.WriteLine("Informe quantos andares existem no prédio: ");
+            totalAndares = int.Parse(Console.ReadLine());
+        }
+
+        public void Entrar() //esta função fará uma pessoa ser adicionada ao elevador.
+        {
+            numeroPessoas = numeroPessoas + 1;
+            if (numeroPessoas == 1)
+            {
+                Console.WriteLine("Você entrou no elevador. Existe 1 pessoa no elevador.");
+            }
+            else if (numeroPessoas > lotacao)
+            {
+                Console.WriteLine($"O número máximo de pessoas foi atingido, você não pode entrar no elevador. Existem {lotacao} pessoas no elevador.");
+                numeroPessoas = lotacao;
+            }
+            else
+            {
+                Console.WriteLine($"Você entrou no elevador. Existem {numeroPessoas} pessoas no elevador.");
+            }
+        }
+
+        public void Sair() //esta função fará uma pessoa ser retirada do elevador.
+        {
+            numeroPessoas = numeroPessoas - 1;
+            if (numeroPessoas < 0)
+            {
+                Console.WriteLine("Não existe ninguém no elevador para ser removido.");
+                numeroPessoas = 0;
+            }
+            else if (numeroPessoas == 0)
+            {
+                Console.WriteLine($"Você saiu do elevador. O elevador está vazio.");
+            }
+            else if (numeroPessoas == 1)
+            {
+                Console.WriteLine($"Você saiu do elevador. Existe {numeroPessoas} pessoa no elevador.");
+            }
+            else
+            {
+                Console.WriteLine($"Você saiu do elevador. Existem {numeroPessoas} pessoas no elevador.");
+            }
+        }
+        
+        public void Subir() //esta função fará o elevador subir um andar. 
+        {
+            andarAtual = andarAtual + 1;
+            if (andarAtual <= totalAndares)
+            {
+                Console.WriteLine($"Você subiu para o {andarAtual}° andar.");
+            }
+            else
+            {
+                Console.WriteLine($"Você atingiu o andar máximo e não pode mais subir. Você está no {totalAndares}° andar.");
+                andarAtual = totalAndares;
+            }
+        }
+        public void Descer() //esta função fará o elevador descer um andar. 
+        {
+            andarAtual = andarAtual - 1;
+            if (andarAtual > 0)
+            {
+                Console.WriteLine($"Você desceu para o {andarAtual}° andar.");
+            }
+            else if (andarAtual == 0)
+            {
+                Console.WriteLine($"Você está no térreo.");
+                andarAtual = 0;
+            }
+            else
+            {
+                Console.WriteLine($"Você está no térreo, portanto não pode descer.");
+                andarAtual = 0;
+            }
+        }
+    }
+}
---
+++ ProjetoElevador/ProjetoElevador/Program.cs
@@ -0,0 +1,83 @@
+﻿using ProjetoElevador.Model;
+using System;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //PROJETO FINAL: Software para gerenciamento de um elevador.
+            Console.WriteLine("SOFTWARE PARA GERENCIAMENTO DE UM ELEVADOR \n");
+            
+            //criar um objeto elevador a partir da classe Elevador
+            Elevador elevador = new Elevador();
+
+            //função Inicializar para pedir ao usuário a capacidade do elevador e total de andares do prédio
+            elevador.Inicializar();
+
+            bool continuar = true;
+
+            do
+            {
+                //4 opções de escolha para o usuário
+                Console.WriteLine($@"
+                Este prédio possui {elevador.totalAndares} andares e a lotação máxima do elevador é de {elevador.lotacao} pessoas.
+                            Escolha uma opção
+                                    1 - Entrar;
+                                    2 - Sair;
+                                    3 - Subir;
+                                    4 - Descer;
+            ");
+
+                //lê a opção que o usuário escolher e como retorno é ativada uma função.
+                string OpcaoEscolhida = Console.ReadLine();
+
+                Console.Clear();
+
+                // Cada uma das escolhas irá ativar uma função (método).
+                switch (OpcaoEscolhida)
+                {
+                    //Escolhendo 1, uma pessoa será adicionada no elevador.
+                    case "1":
+                        elevador.Entrar();
+                        //Escrevi esta parte do código para informar ao usuário o que está armazenado na classe Elevador, a cada ação que ele tomar.
+                        Console.WriteLine($"" +
+                            $"\n Informações do elevador:" +
+                                $" \n   Andar atual: {elevador.andarAtual}" +
+                                $" \n   Quantidade de pessoas no elevador: {elevador.numeroPessoas}"); break;
+                    //Escolhendo 2, uma pessoa será retirada do elevador.
+                    case "2":
+                        elevador.Sair();
+                        Console.WriteLine($"" +
+                            $"\n Informações do elevador:" +
+                                $" \n   Andar atual: {elevador.andarAtual}" +
+                                $" \n   Quantidade de pessoas no elevador: {elevador.numeroPessoas}"); break;
+                    //Escolhendo 3, o elevador irá subir um andar.
+                    case "3":
+                        elevador.Subir();
+                        Console.WriteLine($"" +
+                            $"\n Informações do elevador:" +
+                                $" \n   Andar atual: {elevador.andarAtual}" +
+                                $" \n   Quantidade de pessoas no elevador: {elevador.numeroPessoas}"); break;
+                    //Escolhendo 4, o elevador irá descer um andar.
+                    case "4":
+                        elevador.Descer();
+                        Console.WriteLine($"" +
+                            $"\n Informações do elevador:" +
+                                $" \n   Andar atual: {elevador.andarAtual}" +
+                                $" \n   Quantidade de pessoas no elevador: {elevador.numeroPessoas}"); break;
+
+                    //Caso o usuário não escolha um número de 1 a 4, dará um erro, obrigando-o a sempre escolher de 1 a 4.
+                    default: 
+                        Console.WriteLine("Escolha não válida.");
+                        Console.WriteLine($"" +
+                            $"\n Informações do elevador:" +
+                                $" \n   Andar atual: {elevador.andarAtual}" +
+                                $" \n   Quantidade de pessoas no elevador: {elevador.numeroPessoas}");  break;
+                }
+            }
+            while (continuar);
+        }
+    }
+}
